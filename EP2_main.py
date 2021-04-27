b2b5ed8f1def2956d9415d47f4e0c8000383394e
Rafael Paolino <rafaelpp3@al.insper.edu.br>
2021-04-27 18:56:48 -0300

    prim. mudancas, parece funcionar, faltam + testes

--- EP2_main.py
+++ EP2_main.py
@@ -76,6 +76,15 @@ def jogaroutra():
         else:
             print("responda com s ou n")
 
+
+def podeserint(n):
+    try: 
+        int(n)
+        return True
+    except ValueError:
+        return False
+
+
 def main(baralho):
     ba = baralho
     jogando = True
@@ -89,17 +98,21 @@ def main(baralho):
         i = 0
         for c in ba:
             print(str(i) + ". " + c)
+            i += 1
+            time.sleep(0.01)
         cartavalida = False
         while not cartavalida:
             qcm = input("Qual carta deseja mover? (use o numero a equerda): ")
-            if qcm in range(len(ba)):
-                movpos = lista_movimentos_possiveis(ba, qcm)
-                if movpos == []:
-                    print("nao ha movimentos possiveis para esta carta")
+            if podeserint(qcm):
+                co = int(qcm)
+                if co in range(len(ba)):
+                    movpos = lista_movimentos_possiveis(ba, co)
+                    if movpos == []:
+                        print("nao ha movimentos possiveis para esta carta")
+                    else:
+                        cartavalida = True
                 else:
-                    cartavalida = True
-            else:
-                print("carta invalida")
+                    print("posicao invalida")
         movimentovalido = False
         while not movimentovalido:
             impmov = input("Colocar na 1 ou 3 carta anterior? (1/3): ")
@@ -108,11 +121,11 @@ def main(baralho):
             else:
                 intimpmov = int(impmov)
                 if intimpmov in movpos:
-                    destino = qcm - intimpmov
+                    destino = co - intimpmov
                     movimentovalido = True
                 else:
                     print("este movimento nao e possivel (tente o outro)")
-        empilha(ba, qcm, destino)
+        empilha(ba, co, destino)
 
 
 ##########################################
@@ -120,13 +133,13 @@ def main(baralho):
 # bienvenidos
 
 print("Bem vindo ao paciência acordeão!")
-time.sleep(2)
+time.sleep(1)
 print("Neste jogo voce deve empilhar as cartas ate ter apenas uma pilha.")
-time.sleep(2)
+time.sleep(2.5)
 print("Voce pode empilhar uma carta em seu vizinho anterior, ou na carta 3 casa antes.")
-time.sleep(2)
+time.sleep(2.5)
 print("Para empilhar, ambas as cartas devem ter o mesmo naipe OU o mesmo valor.")
-time.sleep(2)
+time.sleep(2.5)
 
 # jogo mesmo
 
